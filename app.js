c6887cfa891da678be65462ef916b12e7168b52e
easybarezi20 <easybarezi20@gmail.com>
2022-11-15 14:46:40 -0800

    made two arrays with the ammount of bombs and amount of safe spaces

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 const grid = document.querySelector('.grid')
 let width = 10
+let bombAmount = 20
 let squares = []
 
     //function to create the baord 
     function createBoard() {
+        //shuffle bombs on the grid
+        //creates two arrays one with 20 bombs and one with 80 safe spaces
+        const bombArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('safe')
+        console.log(bombArray)
+        console.log(emptyArray)
+
+        //creates the grid baord while also storing each div into an array
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
@@ -13,3 +22,5 @@ let squares = []
         }
     }
     createBoard()
+
+    console.log(squares)
