3b78d25a383494553bf728fcfbbc8bf29271f3ed
easybarezi20 <easybarezi20@gmail.com>
2022-11-16 17:11:42 -0800

    added event listener when you clikc on safe space it logs safe and bomb spcaes log bomb

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 ### Pseudocode for the overall gameplay:
     Overall steps;
     1.generate a 10x10 grid 
-        - 
+        - automate the rid process 
+        -figure out how to populate the grid with values 
+            - use an array to populate the board with a for loop
     2.figure out how to randomiz the ammount of bombs on the grid 
+        - sort() and math.random randomizes an array
     3.figure out how to display the numbers if you choose a safe spot 
-        -figure out how to diplsay amount of bombs around a safe space
+        -
     4.if you hit a mine have the game be over 
         -flag feature if you think bomb is on the square
     5.win when you uncover all squeres without hitting a mine
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 const grid = document.querySelector('.grid')
-let width = 10
 let bombAmount = 20
 let squares = []
 
@@ -9,7 +8,7 @@ let squares = []
         //shuffle bombs on the grid
         //creates two arrays one with 20 bombs and one with 80 safe spaces
         const bombArray = Array(bombAmount).fill('bomb')
-        const emptyArray = Array(width*width - bombAmount).fill('safe')
+        const emptyArray = Array(100 - bombAmount).fill('safe')
         console.log(bombArray)
         console.log(emptyArray)
         //joining the two bomb and safe arrays into one
@@ -20,14 +19,30 @@ let squares = []
         console.log(shuffledArray)
 
         //creates the grid baord while also storing each div into an array
-        for (let i = 0; i < width * width; i++) {
+        for (let i = 0; i < 100; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
             square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
         }
+        // function to figure out how to add a number to the square. 
+        // will be reasigning a nwe type to the divs with the total number of bombs around them.
+
+
     }
     createBoard()
+    // function automateClick() {
+    //     for (let i = 0; i < 100; i++){
+            const safeBtn = document.querySelectorAll(".safe")
+            const bombBtn = document.querySelectorAll(".bomb") 
+            console.log(safeBtn)
+            safeBtn.forEach( cell => { cell.addEventListener('click', () => {console.log('safe')})})
+            bombBtn.forEach( cell => { cell.addEventListener('click', () => {console.log('bomb')})})
+    //     }
+    // }
+    // automateClick()
 
-    
+
+   //automate event listener 
+// 
